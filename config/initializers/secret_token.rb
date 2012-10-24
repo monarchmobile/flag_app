5b5d1bd0e7909c1ae659f40c6f7c4a2a66607fe7
Andrew Gross <monarchmobile@gmail.com>
2012-10-24 13:30:20 -0400

    environment set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlagApp::Application.config.secret_token = 'd20e5e58efdbf5ef93d6f5d5167139fc0fd389e733ddca364633a367d8d4848c327c64dbaaedda4f6161571ae0fb19544edae8ab16c0b6a44ade074788532eb6'
