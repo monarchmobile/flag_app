b3608d01d4f269b47b9fd94561b4ad826d42bc7e
Andrew Gross <monarchmobile@gmail.com>
2012-10-25 17:20:39 -0400

    begginings of image uploader

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,6 @@
 class Image < ActiveRecord::Base
-  attr_accessible :content, :date_taken, :month, :title, :url, :user_id, :week, :year
+  attr_accessible :content, :date_taken, :month, :title, :url, :user_id, :week, :year, :image
   
   belongs_to :user
+  #mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121025200648) do
+ActiveRecord::Schema.define(:version => 20121025211010) do
 
   create_table "images", :force => true do |t|
     t.integer  "user_id"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20121025200648) do
     t.boolean  "year"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "image"
   end
 
   create_table "users", :force => true do |t|
