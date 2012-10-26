81111e8c9515aba19aae3249b197ae27a4cb5cfd
Andrew Gross <monarchmobile@gmail.com>
2012-10-26 15:55:33 -0400

    scrap page:day: 5pics with delete capability

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -45,7 +45,7 @@ class ImagesController < ApplicationController
 
     respond_to do |format|
       if @image.save
-        format.html { redirect_to root_path(@user.id), notice: 'Image was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Image was successfully created.' }
         format.json { render json: @image, status: :created, location: @image }
       else
         format.html { render action: "new" }
