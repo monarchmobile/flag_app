510bd5476b9039b46635d727c83b5d399e74c301
Rodman Henley <rodmanhe@hotmail.com>
2012-12-21 15:01:08 -0500

    added thor

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ end
 
 env_config = File.expand_path('../application.yml', __FILE__)
 if File.exists?(env_config)
-    config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+    config = YAML.load(File.read(env_config))
     config.merge! config.fetch(Rails.env, {})
     config.each do |key, value|
       ENV[key] = value unless value.kind_of? Hash
