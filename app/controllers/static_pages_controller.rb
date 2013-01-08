3044941026aecb5db3cb0675e0df265b6112ae77
Rodman Henley <rodmanhe@hotmail.com>
2013-01-08 14:37:46 -0500

    put home back in

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,10 @@ class StaticPagesController < ApplicationController
   before_filter :current_user, :except => [:home]
   include ApplicationHelper
 
+  def home 
+    @page = page_requested
+  end
+
   def profile
     @page = page_requested
     redirect_to create_guest_path(page: @page) unless current_user
