0d4d048ed4f8860fc7a98da84b05a9c9e416de0c
Andrew Gross <monarchmobile@gmail.com>
2013-03-25 16:52:24 -0400

    fixed image uploader problem

--- app/uploaders/ckeditor_picture_uploader.rb
+++ app/uploaders/ckeditor_picture_uploader.rb
@@ -8,7 +8,9 @@ class CkeditorPictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::ImageScience
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :fog
+  include CarrierWave::MimeTypes
+  process :set_content_type
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
