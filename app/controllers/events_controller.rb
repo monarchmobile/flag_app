f5dbffb3a527078f862ae7b00a4164b7ac156f6c
Rodman Henley <rodmanhe@hotmail.com>
2013-03-28 13:27:19 -0400

    event and announcement, fixed fields for status on form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,16 +1,15 @@
 class EventsController < ApplicationController  
 
-	layout :resolve_layout
-	def new
-		@event = Event.new
-	end
-
-	def show
-		find_event
-	end
+	layout :resolve_layout 
 
 	def index
 		@events = Event.all
+		status_vars
+	end
+
+	def new
+		@event = Event.new
+		status_vars
 	end
 
 	def create
@@ -20,16 +19,19 @@ class EventsController < ApplicationController
 				format.html { redirect_to @event, :notice => "event has been save!" }
 				format.js
 			else
-				format.html { render 'new', notice: "event did not save"}
+				format.html { redirect_to new_event_path, :notice => "You must fill out all required fields"}
 				format.js
 			end
 		end
 	end
 
+	def show
+		find_event
+	end
+
 	def edit
 		find_event
-		@statusable = @event
-		@statuses = Status.all
+		status_vars
 
 	end
 
@@ -51,13 +53,18 @@ class EventsController < ApplicationController
 		@event.status_ids=[]
 		@event.destroy
 		respond_to do |format|
-			format.html {redirect_to dashboard_path, "#{@event.title} deleted succesfully"}
+			format.html {redirect_to dashboard_path}
 			format.js
 		end
 	end
 
 	def find_event
 		@event = Event.find(params[:id])
+	end
+
+	def status_vars
+		@statusable = @event
+		@statuses = Status.all
 	end
 
 
