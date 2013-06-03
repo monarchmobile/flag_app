05bedfda3f6a3f5bfb31996dff634818af2f5fda
Rodman Henley <rodmanhe@hotmail.com>
2013-06-03 13:08:42 -0400

    permit pages for all

--- app/models/statuses_statusable.rb
+++
@@ -1,6 +0,0 @@
-class StatusesStatusable < ActiveRecord::Base
-	attr_accessible :status_id, :statusable_id, :statusable_type
-
-  belongs_to :status 
-  belongs_to :statusable, :polymorphic => true
-end
